8c3c12909dc243091094a85b383af23992c3acb6
Mattias Holmlund <mattias.holmlund@netinsight.net>
2019-06-12 07:35:26 +0200

    Use realistic request length

--- client.ts
+++ client.ts
@@ -18,13 +18,15 @@ function createClient() {
         'https://mattias-http2-1224252573.eu-central-1.elb.amazonaws.com/',
         opt,
     )
-    client.on('error', err => console.error(err))
+    client.on('error', err => console.error('error', err))
     return client
 }
 
 function makeRequest(client: http2.ClientHttp2Session) {
     return new Promise(resolve => {
-        const req = client.request({ ':path': `/delay/${SERVER_DELAY}` })
+        const req = client.request({
+            ':path': `/delay/${SERVER_DELAY}?bt=0;avail_num=12;dur=120000;cid=9kuh123459087612;ip=127.12.43.178;did=00cd62ee-d10a-40ee-93d1-793364cab486;dcat=phone;dplat=ios;uagent=Mozilla%2F5.0%20%28Macintosh%3B%20Intel%20Mac%20OS%20X%2010_14_5%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F74.0.3729.169%20Safari%2F537.36;vid=h274s73nh6;prid=abcd123;fallback=true;rt=vast3;ptner=fgh;geoc=XX`,
+        })
 
         req.on('response', (headers, flags) => {
             // for (const name in headers) {
@@ -38,6 +40,7 @@ function makeRequest(client: http2.ClientHttp2Session) {
             data += chunk
         })
         req.on('end', () => {
+            // console.log(`data: '${data}'`)
             resolve(JSON.parse(data))
         })
         req.end()
@@ -73,7 +76,7 @@ async function run() {
     }
 
     await Promise.all(promises)
-    console.log(Date.now() - start)
+    console.log('timing', Date.now() - start)
     for (let client of clients) {
         client.close()
     }
