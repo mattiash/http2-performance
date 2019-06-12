aff6b227e6adf6aa003be7039dd0188e021256e6
Mattias Holmlund <mattias.holmlund@netinsight.net>
2019-06-12 16:24:51 +0200

    Refactor into AspRequester class

---
+++ asp-requester-performance.ts
@@ -0,0 +1,52 @@
+import { AspRequester } from './asp-requester'
+
+const CONNECTIONS = 3
+const MAX_OUTSTANDING = 120
+const SERVER_DELAY = 150
+const NUM_REQUESTS = 1500
+
+function sleep(ms: number) {
+    return new Promise(resolve => {
+        setTimeout(resolve, ms)
+    })
+}
+
+async function run() {
+    let m = new AspRequester(
+        'https://mattias-http2-1224252573.eu-central-1.elb.amazonaws.com/',
+        CONNECTIONS,
+        MAX_OUTSTANDING,
+    )
+
+    await sleep(1000)
+
+    while (true) {
+        const start = Date.now()
+        let promises = new Array<Promise<object>>()
+        for (let c = 0; c < NUM_REQUESTS; c++) {
+            promises.push(
+                m
+                    .addRequest(
+                        `/delay/${SERVER_DELAY}?bt=0;avail_num=12;dur=120000;cid=9kuh123459087612;ip=127.12.43.178;did=00cd62ee-d10a-40ee-93d1-793364cab486;dcat=phone;dplat=ios;uagent=Mozilla%2F5.0%20%28Macintosh%3B%20Intel%20Mac%20OS%20X%2010_14_5%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F74.0.3729.169%20Safari%2F537.36;vid=h274s73nh6;prid=abcd123;fallback=true;rt=vast3;ptner=fgh;geoc=XX`,
+                    )
+                    .catch(() => {
+                        console.log('caught')
+                        return {}
+                    }),
+            )
+        }
+
+        // globalSession.close()
+
+        await Promise.all(promises)
+        console.log('timing', Date.now() - start)
+
+        await sleep(10 * 60 * 1000)
+    }
+
+    // for (let client of clients) {
+    //     client.close()
+    // }
+}
+
+run()
--- asp-requester.ts
+++ asp-requester.ts
@@ -1,10 +1,7 @@
 import * as http2 from 'http2'
 import { Deferred } from './deferred'
-
-const SERVER_DELAY = 150
-const NUM_REQUESTS = 1500
-const MAX_OUTSTANDING = 120
-const CONNECTIONS = 3
+import * as dbg from 'debug'
+const debug = dbg('http2-session')
 
 const opt: http2.SecureClientSessionOptions = {
     rejectUnauthorized: false,
@@ -15,18 +12,18 @@ interface SessionRequest {
     deferred: Deferred<object>
 }
 
-export class Session {
+class Session {
     private client?: http2.ClientHttp2Session
     private outstandingRequests = 0
     private pingInterval: NodeJS.Timeout
 
-    constructor(private basePath: string) {
+    constructor(private basePath: string, private maxOutstanding: number) {
         this.createClient()
         this.pingInterval = setInterval(() => {
             if (this.client) {
                 this.client.ping((err, duration) => {
                     if (err) {
-                        console.log('ping err after ' + duration + ' ms')
+                        debug('ping err after ' + duration + ' ms')
                     }
                 })
             }
@@ -37,24 +34,24 @@ export class Session {
         const client = http2.connect(this.basePath, opt)
         client.on('connect', () => {
             this.client = client
-            console.log(new Date() + ' connect')
+            debug('connect')
         })
-        client.on('error', arg => console.log(new Date() + ' error', arg))
+        client.on('error', arg => debug('error', arg))
         client.on('close', (arg: any) => {
-            console.log(new Date() + ' close', arg)
+            debug('close', arg)
             this.client = undefined
             this.createClient()
         })
-        client.on('ping', () => console.log(new Date() + ' ping'))
-        client.on('goaway', arg => console.log(new Date() + ' goaway', arg))
+        client.on('ping', () => debug('ping'))
+        client.on('goaway', arg => debug('goaway', arg))
     }
 
     get isBusy() {
-        return this.outstandingRequests >= MAX_OUTSTANDING
+        return this.outstandingRequests >= this.maxOutstanding
     }
 
     addRequest(url: string, deferred: Deferred<object>) {
-        if (this.outstandingRequests < MAX_OUTSTANDING) {
+        if (this.outstandingRequests < this.maxOutstanding) {
             return this.doRequest(url, deferred)
         } else {
             return false
@@ -62,7 +59,7 @@ export class Session {
     }
 
     private doRequest(path: string, deferred: Deferred<object>) {
-        if (this.client) {
+        if (this.client && !this.client.closed) {
             let rejected = false
             this.outstandingRequests++
             const req = this.client.request({
@@ -88,7 +85,11 @@ export class Session {
             req.on('end', () => {
                 if (!rejected) {
                     this.outstandingRequests--
-                    deferred.resolve(JSON.parse(data))
+                    try {
+                        deferred.resolve(JSON.parse(data))
+                    } catch (e) {
+                        deferred.reject(new Error('Failed to parse json'))
+                    }
                 }
             })
             req.end()
@@ -99,13 +100,17 @@ export class Session {
     }
 }
 
-export class MultiSession {
+export class AspRequester {
     private nextSession = 0
     private sessions = new Array<Session>()
     private unallocatedRequests = new Array<SessionRequest>()
-    constructor(basePath: string, parallelSessions: number) {
+    constructor(
+        basePath: string,
+        parallelSessions: number,
+        maxOutstanding: number,
+    ) {
         for (let c = 0; c < parallelSessions; c++) {
-            this.sessions.push(new Session(basePath))
+            this.sessions.push(new Session(basePath, maxOutstanding))
         }
     }
 
@@ -140,46 +145,3 @@ export class MultiSession {
         }
     }
 }
-
-function sleep(ms: number) {
-    return new Promise(resolve => {
-        setTimeout(resolve, ms)
-    })
-}
-
-async function run() {
-    let m = new MultiSession(
-        'https://mattias-http2-1224252573.eu-central-1.elb.amazonaws.com/',
-        CONNECTIONS,
-    )
-
-    await sleep(1000)
-
-    while (true) {
-        const start = Date.now()
-        let promises = new Array<Promise<object>>()
-        for (let c = 0; c < NUM_REQUESTS; c++) {
-            promises.push(
-                m
-                    .addRequest(
-                        `/delay/${SERVER_DELAY}?bt=0;avail_num=12;dur=120000;cid=9kuh123459087612;ip=127.12.43.178;did=00cd62ee-d10a-40ee-93d1-793364cab486;dcat=phone;dplat=ios;uagent=Mozilla%2F5.0%20%28Macintosh%3B%20Intel%20Mac%20OS%20X%2010_14_5%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F74.0.3729.169%20Safari%2F537.36;vid=h274s73nh6;prid=abcd123;fallback=true;rt=vast3;ptner=fgh;geoc=XX`,
-                    )
-                    .catch(() => {
-                        console.log('caught')
-                        return {}
-                    }),
-            )
-        }
-
-        await Promise.all(promises)
-        console.log('timing', Date.now() - start)
-
-        await sleep(10 * 60 * 1000)
-    }
-
-    // for (let client of clients) {
-    //     client.close()
-    // }
-}
-
-run()
